1063f2be7a47a864e5e10593b8c459dd0c97ac13
Bart Stéphane <stephane.bart@he-arc.ch>
2020-07-07 23:25:33 +0200

    reset appConfig.js

--- public/appConfig.js
+++ public/appConfig.js
@@ -3,11 +3,11 @@ window.kursausschreibung.appConfig = {
   // the token type
   "tokenType": "urn:ietf:params:oauth:token-type:jwt-bearer",
   // API base URL without trailing slash
-  "apiUrl": "https://evento-test.erz.be.ch",
+  "apiUrl": "https://eventotest.api",
   // base Url of the web application without trailing slash (also the redirect url for public clients)
-  //"webBaseUrl": "www.test.com",
+  "webBaseUrl": "https://test.com",
   // base url of the CLX.Evento OAuth Server.
-  "oauthUrl": "https://evento-test.erz.be.ch/OAuth",
+  "oauthUrl": "https://eventotest.oauth",
   // the instance id for this application.
   "instanceId": "GymBiel",
   // the client id for this application.
